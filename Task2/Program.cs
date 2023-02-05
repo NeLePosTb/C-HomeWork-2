7d2bdfd631a1b2be5d151d7866637d457f724a92
Vlad <vmereskin@gmail.com>
2023-02-05 03:45:06 +0300

    Add Task 1

---
+++ Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿bool isWork = true;
+
+while(isWork)
+{
+    Console.Write("Введите номер задания ");
+
+    if (int.TryParse(Console.ReadLine(), out int i))
+    {
+        switch(i)
+        {
+            case 1:
+            {
+                Console.Write("Введите трехзначное число ");
+
+                int a = int.Parse(Console.ReadLine());
+                int b = a / 10 % 10;
+                System.Console.WriteLine(b);
+
+                break;
+            }
+
+            case 2:
+            {
+                System.Console.Write("Введите число ");
+
+                int a =int.Parse(Console.ReadLine());
+                
+                break;
+            }
+
+            case 0: isWork = false; break;
+        }
+    }
+}
