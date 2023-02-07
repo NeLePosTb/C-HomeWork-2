afb7c176e72ab48dee9a5983175f971989c17b46
Vlad <vmereskin@gmail.com>
2023-02-07 16:04:22 +0300

    Add Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -67,9 +67,18 @@ while (isWork)
                 //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
                 System.Console.Write("Введите число: ");
-                int number =int.Parse(Console.ReadLine());
+                int number = int.Parse(Console.ReadLine());
 
+                Console.WriteLine($"Куб числа {number} = {cube(number)}");
                 
+
+                int cube (int N)
+                {
+                    N = number * number * number;
+                    return N;
+                }
+
+
                 break;
             }
 
