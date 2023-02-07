0f1bc7be3025b332c5165ee33105c7897792afac
Vlad <vmereskin@gmail.com>
2023-02-07 15:54:20 +0300

    Add Task2

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -34,6 +34,45 @@ while (isWork)
                 break;
             }
 
+            case 2:
+            {
+                //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+                Console.WriteLine("введите координаты первой точки: ");
+                System.Console.Write("X = ");
+                int.TryParse(Console.ReadLine(), out int x1);
+                System.Console.Write("Y = ");
+                int.TryParse(Console.ReadLine(), out int y1);
+                System.Console.Write("Z = ");
+                int.TryParse(Console.ReadLine(), out int z1);
+                Console.WriteLine("введите координаты второй точки: ");
+                System.Console.Write("X = ");
+                int.TryParse(Console.ReadLine(), out int x2);
+                System.Console.Write("Y = ");
+                int.TryParse(Console.ReadLine(), out int y2);
+                System.Console.Write("Z = ");
+                int.TryParse(Console.ReadLine(), out int z2);
+
+                double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+                Console.Write("Дистанция = ");
+                System.Console.WriteLine(Math.Round(distance, 3));
+                
+
+
+                break;
+            }
+
+            case 3:
+            {
+                //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+                System.Console.Write("Введите число: ");
+                int number =int.Parse(Console.ReadLine());
+
+                
+                break;
+            }
+
             case 0: isWork = false; break;
         }
     }
