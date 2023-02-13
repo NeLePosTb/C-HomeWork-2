0532e62f235284b305cd1e05eb6c8a7777e8e428
Vlad <vmereskin@gmail.com>
2023-02-13 14:27:31 +0300

    Add Task 2

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -18,14 +18,37 @@ while(isWork)
 
                 Console.WriteLine($"Степень {degree} числа {number} = {up(number)}");
 
-                int up (int count)
+                int up (int N)
                 {
-                    int N = number;
+                    N = number;
                     for (int i = 1; i < degree; i++)
                     {
                         N *= number;
                     } 
                     return N;
+                }
+
+
+                break;
+            }
+
+            case 2:
+            {
+                //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+                System.Console.Write("Введите число: ");
+                int number = int.Parse(Console.ReadLine());
+                System.Console.WriteLine(sum(number));
+
+                int sum (int N)
+                {
+                   N = 0;
+                   while (number > 0)
+                   {
+                    N = N + number % 10;
+                    number = number / 10;
+                   }
+                   return N;
                 }
 
 
